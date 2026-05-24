05a2189285ecb3cba70fa9dd44bf4d1b13d2f7e4
salitha <salitha.herath@spemai.com>
2026-05-24 09:40:53 +0530

    Force HTTPS behind Azure load balancer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Azure App Service terminates TLS at the load balancer; trust forwarded headers.
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
